c9d2bb2b19ef491bd69d6180ad0cc30307b963ba
Jack Gomez <jgomez26@students.polytechnic.org>
2026-04-05 11:50:17 -0700

    AND added fields to store passenger information and unique ID counter

--- Passenger.java
+++ Passenger.java
@@ -1,3 +1,15 @@
 public class Passenger {
     
-}
+//fields
+
+    private int startStation;
+    private int destination;
+    private int location;
+    private int originalDestination;
+    private boolean direction;
+    private int ID;
+    private static int globalID = 0;
+
+    
+
+
