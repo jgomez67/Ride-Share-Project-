06c9a4fc884933f11dc987d196dd7b7327bb418b
Jack Gomez <jgomez26@students.polytechnic.org>
2026-04-05 11:54:39 -0700

    added constructor to initialize passenger data and assign ID

--- Passenger.java
+++ Passenger.java
@@ -10,6 +10,19 @@ public class Passenger {
     private int ID;
     private static int globalID = 0;
 
-    
+//constructors
+
+public Passenger(int startStation, int destination) {
+    this.startStation = startStation;
+    this.destination = destination;
+    this.originalDestination = destination;
+    this.location = startStation;
+direction = startStation < destination;
+ID = globalID++;
+
+}
+
+
+
 
 
