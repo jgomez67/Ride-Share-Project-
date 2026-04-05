0e2f0356291f9d3c945504814e8f009430ecb89b
Jack Gomez <jgomez26@students.polytechnic.org>
2026-04-05 12:42:10 -0700

    Added constructor and methods to manage passengers at each station

--- Station.java
+++ Station.java
@@ -6,3 +6,33 @@ public class Station {
     //fields
     private int stationNumber;
     private ArrayList<Passenger> people;
+    
+    //constructors
+
+public Station(int stationNumber) {
+
+   this.stationNumber = stationNumber;
+   this.people = new ArrayList<>();
+}
+
+   public int getStationNumber() {
+       return stationNumber;
+   }
+
+   public ArrayList<Passenger> getPeople() {
+       return people;
+   }
+
+   public void spawnPerson(Passenger passenger){
+       people.add(passenger);
+   }
+
+   public void removePerson(Passenger passenger) {
+       people.remove(passenger);
+
+   }
+   public void removePerson(int index) {
+       people.remove(index);
+   }
+
+}
