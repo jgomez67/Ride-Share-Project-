7979c4ea5b6d5e61dae451c1828cc8993526757a
Jack Gomez <jgomez26@students.polytechnic.org>
2026-04-05 20:32:54 -0700

    started Road class with fields for cars, stations, passengers, and random generator

---
+++ Road.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class Road{
+    //fields
+    private ArrayList<Car> cars;
+    private ArrayList<Station> stations;
+    private Random rand;
+    private int deliveredPassengers;
+    private int totalPassengers;
+
+    //constructors
+    
+    }
+}
